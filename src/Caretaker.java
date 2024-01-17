f0e8f35de7e2a92200c8480c07621229012e00ae
Rodrigo Fontes <rodrigofontes377@gmail.com>
2024-01-17 10:16:28 -0300

    .

--- src/Caretaker.java
+++ src/Caretaker.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Caretaker {
-    
+    private List<Memento> mementoList = new ArrayList<>();
+
+    public void addMemento(Memento memento) {
+        mementoList.add(memento);
+    }
+
+    public Memento getMemento(int index) {
+        return mementoList.get(index);
+    }
 }
